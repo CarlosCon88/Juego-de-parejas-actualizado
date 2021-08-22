b426192ef541fb17ace904bd2a880f0e04e7d367
Carlos Contreras <cdcontreras43@gmail.com>
2021-08-22 17:12:18 -0600

    Agregar archivos de proyecto.

---
+++ Juego de parejas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juego_de_parejas
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+public partial class Form1 : Form
+{
+    // Use this Random object to choose random icons for the squares
+    Random random = new Random();
+
+    // Each of these letters is an interesting icon
+    // in the Webdings font,
+    // and each icon appears twice in this list
+    List<string> icons = new List<string>()
+    {
+        "!", "!", "N", "N", ",", ",", "k", "k",
+        "b", "b", "v", "v", "w", "w", "z", "z"
+    };
+
+
+
+
